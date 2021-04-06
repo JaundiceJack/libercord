0300b8dba22839cd52d3252bfc21b9910c34487e
Jame McNeilan <jamesmcneilan@protonmail.com>
2021-04-05 19:59:04 -0600

    nomob

--- client/src/store.js
+++ client/src/store.js
@@ -3,10 +3,13 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
+const initialState = {};
+
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
+  initialState,
   compose(
     applyMiddleware(...middleware),
     window.navigator.userAgent.includes('Chrome') ?
@@ -14,7 +17,6 @@ const store = createStore(
       compose,
     ),
   );
-
 
 //const store = createStore(rootReducer, initialState,
 //  compose(
